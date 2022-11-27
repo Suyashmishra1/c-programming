f2803c528f2314e9da7d1dcca627306402558c45
Suyashmishra1 <119160595+Suyashmishra1@users.noreply.github.com>
2022-11-27 15:41:00 +0530

    Update hello world.c

--- hello world.c
+++ hello world.c
@@ -3,10 +3,8 @@ int main(){
     int a,b;
     printf("enter the number a =")
     scanf(%d,&a);
-    
     printf("enter the number b =")
     scanf(%d,&b);
     printf("the sum of the number is =",a+b);
-
     return 0;
 }
